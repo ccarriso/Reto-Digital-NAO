e152cc3d28e3f990ae19e763a42a137f5d602285
ccarriso <151290759+ccarriso@users.noreply.github.com>
2024-04-02 00:02:25 -0600

    Add files via upload

---
+++ validar.js
@@ -0,0 +1,16 @@
+/*function validarCorreo() {
+    var emailDato = document.getElementById('email');
+
+    if (emailDato && emailDato.value) {
+        var emailValido = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+        if (emailValido.test(emailDato.value)) {
+            
+            alert('Sus datos han sido registrados');
+        } else {
+            alert('Email is invalid');
+        }
+    } else {
+        alert('Email field is empty');
+    }
+}*/
